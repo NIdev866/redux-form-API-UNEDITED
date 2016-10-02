3bd980ee004f9a0c21609442702629b985a6147d
PiotrBerebecki <petebere@gmail.com>
2016-10-02 16:16:57 +0100

    Add logic for checking if user already exists in the database

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,28 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  // Check if user exist
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+    
+    // If user exists, return error
+    if (existingUser) { 
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+    
+    // If user does not exist, create user
+    const user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err) {
+      if (err) { return next(err); }
+    });
+  
+    // Respond
+    res.json({ success: true });
+  });
 };
