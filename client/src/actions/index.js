ea5d9b934c271d7a66af51da6aa354dc7d3060ba
PiotrBerebecki <petebere@gmail.com>
2016-10-04 16:59:08 +0100

    Redirect user to feature upon successful sign in

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -6,13 +7,20 @@ const ROOT_URL = 'http://localhost:3090';
 export function signinUser({ email, password }) {
   return function(dispatch) {
     // Submit email/password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-    
-    // If request is good:
-    // - Update state to indicate user is auth'd
-    // - Save the JWT token
-    // - Redirect to route '/feature'
-    // If request is bad:
-    // - Show error to user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good:
+        // - Update state to indicate user is auth'd
+        
+        // - Save the JWT token
+        
+        // - Redirect to route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(err => {
+        // If request is bad:
+        // - Show error to user
+        
+      });
   };
 }
