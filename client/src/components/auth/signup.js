cac47ebf1bca18d4c83abd556c353a4a9fcfe24c
PiotrBerebecki <petebere@gmail.com>
2016-10-05 17:08:01 +0100

    Add validation if password and password confirm do not match

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -4,11 +4,42 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
   render() {
+    const { handleSubmit,
+            fields : { email, password, passwordConfirm }} = this.props;
+            
     return (
-      <div>Signup</div>
+      <form onSubmit="">
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input {...email} className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input {...password} type="password" className="form-control" />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input {...passwordConfirm} type="password" className="form-control" />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign up</button>
+      </form>
     );
   }
 }
 
-export default Signup;
+function validate(formProps) {
+  const errors = {};
+  
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+  
+  return errors;
+}
 
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
+})(Signup);
